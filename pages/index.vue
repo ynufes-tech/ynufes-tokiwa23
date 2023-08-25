ef962923fb4705b91fdb3f60c19e7dbbefc921a0
Shion Ichikawa <shion1305@gmail.com>
2023-08-26 00:31:11 +0900

    🚧 Top page

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'home'
+})
+</script>
+
+<template>
+  <div class="page-root">
+    <div id="logo-area">
+      <h1>常盤祭2023</h1>
+      <h2>横浜国立大学 常盤祭</h2>
+      <hr/>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+#logo-area {
+  font-size: min(16px, 4vw);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-self: flex-start;
+  margin: .6em;
+
+  > h1 {
+    font-size: 2.5em;
+    font-weight: 500;
+    color: #1d2630;
+    margin: .5em .5em .2em;
+  }
+
+  > h2 {
+    font-size: 1.2em;
+    font-weight: 400;
+    margin: .5em 1em 1em 1em;
+  }
+
+  > hr {
+    width: 100%;
+    margin: 0;
+    border: 1px solid #1d2630;
+  }
+}
+
+.page-root {
+  display: flex;
+  flex-direction: column;
+}
+</style>

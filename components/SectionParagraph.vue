80d652354ed00f02e6963b926edfccef658fa7db
tomoyahiroe <tomoya4creative@gmail.com>
2023-08-26 20:18:37 +0900

    :sparkles: feat section paragraph component

---
+++ components/SectionParagraph.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+const props = defineProps({
+  textList: {
+    type: Array<string>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+<div class="section-paragraph">
+  <p v-for="(text, index) in props.textList" :key="index">{{ text }}</p>
+</div>
+</template>
+
+
+<style scoped>
+.section-paragraph {
+  line-height: 1.7;
+  font-size: 1.2rem;
+  > p {
+    margin-bottom: 1rem;
+    text-align: justify;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  @media screen and (max-width: 30rem) {
+    > p {
+      font-size: 1rem;
+    }
+    
+  }
+}
+</style>

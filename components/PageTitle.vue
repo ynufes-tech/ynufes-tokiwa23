cc3b93382da5221cc8178fc0460a5aed9e07ab79
tomoyahiroe <tomoya4creative@gmail.com>
2023-08-26 22:57:32 +0900

    WIP tomoya

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -1,111 +1,118 @@
 <script lang="ts" setup>
-const waveCanvas = ref(null)
-onMounted(
-    () =>{
-        console.log(waveCanvas)
-    }
-)
+onMounted(() => {
+  const unit = 100;
+  let canvas: HTMLCanvasElement;
+  const info = {
+    seconds: 0,
+    t: 0,
+  }; // 全キャンバス共通の描画情報
+  const colorList: string[] = ["#8AC6D6", "#87C8A0", "#FDFEB8", "#8CB6DE"]; // 各キャンバスの色情報
+  let contextCache: CanvasRenderingContext2D;
 
-var unit = 100,
-    canvasList, // キャンバスの配列
-    info = {}, // 全キャンバス共通の描画情報
-    colorList; // 各キャンバスの色情報
-function init() {
-    info.seconds = 0;
-    info.t = 0;
-    colorList = [];
+  function init() {
     // canvas1個めの色指定
-    colorList.push(['#8AC6D6', '#87C8A0', '#FDFEB8', '#8CB6DE']);//重ねる波線の色設定
+    canvas = <HTMLCanvasElement>document.getElementById("waveCanvas");
     // 各キャンバスの初期化
-        const canvas = waveCanvas.value
-        canvas.contextCache = canvas.getContext("2d");
+      canvas.width = document.documentElement.clientWidth; //Canvasのwidthをウィンドウの幅に合わせる
+      canvas.height = 200; //波の高さ
+    contextCache = canvas.getContext("2d") ?? new CanvasRenderingContext2D();
     // 共通の更新処理呼び出し
-    update();
-}
+    update(canvas);
+  }
 
-function update() {
-    for(var canvasIndex in canvasList) {
-        var canvas = canvasList[canvasIndex];
-        // 各キャンバスの描画
-        draw(canvas, colorList[canvasIndex]);
-    }
+  const update = (canvas: HTMLCanvasElement) => {
+    Math.random();
+    // 各キャンバスの描画
+    draw(canvas, colorList);
     // 共通の描画情報の更新
-    info.seconds = info.seconds + .014;
-    info.t = info.seconds*Math.PI;
+    info.seconds = info.seconds + 0.014;
+    info.t = info.seconds * Math.PI;
     // 自身の再起呼び出し
     setTimeout(update, 35);
-}
+  };
 
-/**
- * Draw animation function.
- *
- * This function draws one frame of the animation, waits 20ms, and then calls
- * itself again.
- */
-function draw(canvas, color) {
+  /**
+   * Draw animation function.
+   *
+   * This function draws one frame of the animation, waits 20ms, and then calls
+   * itself again.
+   */
+  const draw = (canvas: HTMLCanvasElement, colors: string[]) => {
     // 対象のcanvasのコンテキストを取得
-    var context = canvas.contextCache;
+    const context = contextCache;
     // キャンバスの描画をクリア
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     //波を描画 drawWave(canvas, color[数字（波の数を0から数えて指定）], 透過, 波の幅のzoom,波の開始位置の遅れ )
-    drawWave(canvas, color[0], 0.7, 3, 5);
-    drawWave(canvas, color[1], 0.7, 4, 1.3);
-    drawWave(canvas, color[2], 0.7, 1.6, 0);
-    drawWave(canvas, color[3], 0.7, 3, 0);
-}
+    drawWave(canvas, colors[0], 0.7, 3, 5);
+    drawWave(canvas, colors[1], 0.7, 4, 1.3);
+    drawWave(canvas, colors[2], 0.7, 1.6, 0);
+    drawWave(canvas, colors[3], 0.7, 3, 0);
+  };
 
-/**
- * 波を描画
- * drawWave(色, 不透明度, 波の幅のzoom, 波の開始位置の遅れ)
- */
-function drawWave(canvas, color, alpha, zoom, delay) {
-    var context = canvas.contextCache;
-    context.strokeStyle = color;//線の色
-    context.lineWidth = 100;//線の幅
+  /**
+   * 波を描画
+   * drawWave(色, 不透明度, 波の幅のzoom, 波の開始位置の遅れ)
+   */
+  const drawWave = (
+    canvas: HTMLCanvasElement,
+    color: string,
+    alpha: number,
+    zoom: number,
+    delay: number
+  ) => {
+    const context = contextCache;
+    context.strokeStyle = color; //線の色
+    context.lineWidth = 100; //線の幅
     context.globalAlpha = alpha;
     context.beginPath(); //パスの開始
     drawSine(canvas, info.t / 0.5, zoom, delay);
     context.stroke(); //線
-}
+  };
 
-/**
- * Function to draw sine
- *
- * The sine curve is drawn in 10px segments starting at the origin.
- * drawSine(時間, 波の幅のzoom, 波の開始位置の遅れ)
- */
-function drawSine(canvas, t, zoom, delay) {
-    var xAxis = Math.floor(canvas.height/2);
-    var yAxis = 0;
-    var context = canvas.contextCache;
+  /**
+   * Function to draw sine
+   *
+   * The sine curve is drawn in 10px segments starting at the origin.
+   * drawSine(時間, 波の幅のzoom, 波の開始位置の遅れ)
+   */
+  function drawSine(
+    canvas: HTMLCanvasElement,
+    t: number,
+    zoom: number,
+    delay: number
+  ) {
+    const xAxis = Math.floor(canvas.height / 2);
+    const yAxis = 0;
+    const context = contextCache;
     // Set the initial x and y, starting at 0,0 and translating to the origin on
     // the canvas.
-    var x = t; //時間を横の位置とする
-    var y = Math.sin(x)/zoom;
-    context.moveTo(yAxis, unit*y+xAxis); //スタート位置にパスを置く
+    let x = t; //時間を横の位置とする
+    let y = Math.sin(x) / zoom;
+    context.moveTo(yAxis, unit * y + xAxis); //スタート位置にパスを置く
 
     // Loop to draw segments (横幅の分、波を描画)
-    for (i = yAxis; i <= canvas.width + 10; i += 10) {
-        x = t+(-yAxis+i)/unit/zoom;
-        y = Math.sin(x - delay)/2;
-        context.lineTo(i, unit*y+xAxis);
+    for (let i = yAxis; i <= canvas.width + 10; i += 10) {
+      x = t + (-yAxis + i) / unit / zoom;
+      y = Math.sin(x - delay) / 2;
+      context.lineTo(i, unit * y + xAxis);
     }
-}
+  }
 
-init();
+  init();
+});
 </script>
 
 <template>
   <div class="page-title">
-      <canvas ref="waveCanvas" class="wave"></canvas>
-      <p>apple</p>
+    <canvas id="waveCanvas" class="wave"></canvas>
+    <p>apple</p>
   </div>
 </template>
 
 <style scoped>
-.wave{
-    width: 100svw;
-    height: 200px;
-}
+/* .wave {
+  width: 100svw;
+  height: 200px;
+} */
 </style>

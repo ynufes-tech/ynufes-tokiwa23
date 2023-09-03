e323b3c36fad136dc71a61f7b5bce180c5c295be
hinata0629 <hinata749890@icloud.com>
2023-09-03 10:17:10 +0900

    0123-aboutpageの実装

---
+++ pages/about/index.vue
@@ -0,0 +1,180 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="about-page">
+        <div class="about-title">
+            <page-title title="ABOUT"/>
+            <a href="/"><img src="/images/logo.webp" alt="ynufes-logo" class="logo-image"></a>
+            <p class="menu-button">MENU</p>
+        </div>
+        <div class="tokiwa-about">
+            <section-title text="常盤祭について" class="upper-title" />
+            <h1 class="child-title">常盤祭とは何か</h1>
+            <div class="upper-dashed-line">
+                <a class="line-message">説明文</a>
+            </div>
+            <p class="main-script">
+                適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章
+            </p>
+            <hr class="dashed-line">
+        </div>
+        <div class="theme-about">
+            <section-title text="テーマについて" class="upper-title" />
+            <h1 class="child-title">未来航路</h1>
+            <div class="upper-dashed-line">
+                <a class="line-message">説明文</a>
+            </div>
+            <p class="main-script">
+                適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章
+            </p>
+            <hr class="dashed-line">
+        </div>
+        <div class="committee-about">
+            <section-title text="実行委員会について" class="upper-title" />
+            <div class="last-upper-line">
+                <div class="upper-dashed-line">
+                    <a class="line-message">説明文</a>
+                </div>
+            </div>
+            <p class="main-script">
+                適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章、適当な文章
+            </p>
+            <hr class="dashed-line">
+            <div class="ynufes-photo">
+                <img src="https://placehold.jp/350x350.png" alt="pleceholder" class="committee-photo">
+                <img src="https://placehold.jp/350x350.png" alt="pleceholder" class="committee-photo">
+            </div>
+        </div>
+        <div class="scroll-button">
+            <a href="#" class="gotop">top</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+html{
+    scroll-behavior: smooth;
+}
+.about-page{
+    background-color: #FCF9EF;
+    width: 100vw;
+}
+.logo-image{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: min( 15em , 40svw);
+}
+.menu-button{
+    position: fixed;
+    top: 5svw;
+    right: 5svw;
+    color: #FCF9EF;
+    font-size: 2rem;
+    font-weight: bolder;
+    background-color: #da8080;
+    width: min(4em,30svw);
+    text-align: center;
+    padding:  0.3em 0 ;
+    border-radius: 0.5em;
+}
+.dashed-line{
+    border: none;
+    border-top: #575F6A 2px dashed;
+    width: 75%;
+}
+.upper-dashed-line{
+    width: 75%;
+    display: flex;
+    align-items: center;
+    margin: 1em auto;
+}
+.upper-dashed-line:after {
+    content:"";
+    border-top: #575F6A 2px dashed;
+    flex-grow: 1
+}
+.last-upper-line{
+    margin: 3em 0 1em;
+}
+.line-message{
+    text-align: center;
+    color: #575F6A;
+    font-size: 1rem;
+    font-weight: inherit;
+}
+.child-title{
+    color: #575F6A;
+    font-size: 2rem;
+    font-weight: bold;
+    margin: 0.75em 0 0.75em 12.5%;
+}
+.main-script{
+    width: 75svw;
+    margin: 1rem auto;
+    font-size: 16px;
+}
+.ynufes-photo{
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+.committee-photo{
+    margin: 3em 1.5em;
+    border-radius: 1em;
+}
+.gotop{
+    display: block;
+    width: 60px;
+    height: 60px;
+    box-sizing: border-box;
+    background: #FFF;
+    border: 1px solid #999;
+    border-radius: 1em;
+    padding-top: 30px;
+    text-align: center;
+    letter-spacing: -1px;
+    font-size: 85%;
+    text-decoration: none;
+    color: #333;
+    opacity: 0.5;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    z-index: 10000;
+}
+.gotop::before{
+    content: "";
+    display: block;
+    border-top: 2px solid #333;
+    border-right: 2px solid #333;
+    width: 25%;
+    height: 25%;
+    top: 25%;
+    left: 0;
+    right: 0;
+    margin: auto;
+    position: absolute;
+    transform: rotate(-45deg);
+}
+.gotop:hover{
+    opacity: 1;
+}
+@media(max-width:750px){
+    .gotop{
+        width: 40px;
+        height: 40px;
+        text-indent: -9999px;
+        opacity: 1;
+        border: none;
+        background: none;
+        bottom: 10px;
+        right: 10px;
+    }
+    .gotop::before{
+        bottom: 0;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <a href="/about">リンクへ飛ぶ</a>
+  </div>
+</template>
+
+<style scoped></style>

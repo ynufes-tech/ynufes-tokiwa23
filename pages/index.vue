436a35425f1c84557f9e5846694dca86e25f4737
hinata0629 <hinata749890@icloud.com>
2023-09-06 11:33:35 +0900

    文字のレスポンシブ対応

--- pages/index.vue
+++ pages/index.vue
@@ -12,42 +12,51 @@ useHead({
 
 <template>
   <div class="page-root">
-    <LogoPrime/>
+    <div class="logo-wrapper">
+       <div class="logo">
+           <LogoPrime/>
+       </div>
+    </div>
+    <div class="top-wrapper">
+
+    </div>
     <div id="top-pane">
       <h1 id="top-title">未来航路</h1>
-      <div id="schedule">
-        <h2>開催日時</h2>
-        <table>
-          <!--          <tr>-->
-          <!--            <th>日付</th>-->
-          <!--            <th>時間</th>-->
-          <!--          </tr>-->
-          <tr>
-            <td colspan="3">～Day1～</td>
-          </tr>
-          <tr>
-            <td>11/3</td>
-            <td>(Fri)</td>
-            <td>13:00～21:00</td>
-          </tr>
-          <tr>
-            <td colspan="3">～Day2～</td>
-          </tr>
-          <tr>
-            <td>11/4</td>
-            <td>(Sat)</td>
-            <td>&ensp;9:00～21:00</td>
-          </tr>
-          <tr>
-            <td colspan="3">～Day3～</td>
-          </tr>
-          <tr>
-            <td>11/5</td>
-            <td>(Sun)</td>
-            <td>&ensp;9:00～15:00</td>
-          </tr>
-        </table>
-      </div>
+        <div class="schedule-wrapper">
+            <div id="schedule">
+                <h2>開催日時</h2>
+                <table>
+                    <!--          <tr>-->
+                    <!--            <th>日付</th>-->
+                    <!--            <th>時間</th>-->
+                    <!--          </tr>-->
+                    <tr>
+                        <td colspan="3">～Day1～</td>
+                    </tr>
+                    <tr>
+                        <td>11/3</td>
+                        <td>(Fri)</td>
+                        <td>13:00～21:00</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">～Day2～</td>
+                    </tr>
+                    <tr>
+                        <td>11/4</td>
+                        <td>(Sat)</td>
+                        <td>&ensp;9:00～21:00</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">～Day3～</td>
+                    </tr>
+                    <tr>
+                        <td>11/5</td>
+                        <td>(Sun)</td>
+                        <td>&ensp;9:00～15:00</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
       <BouncingText text="※只今製作中..."/>
       <p id="upcoming-notify"><span>常盤祭に関する最新情報は</span><span>随時公開されていきます</span></p>
     </div>
@@ -60,10 +69,26 @@ useHead({
 .logo-prime {
   font-size: min(16px, 3.5vw);
 }
+.logo{
+  width: fit-content;
+}
+@media screen and (min-width:768px){
+  .logo-wrapper{
+    width: 90svw;
+    margin: 0 auto;
+  }
+  .schedule-wrapper{
+    width: 90svw;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row-reverse;
+  }
+}
 
 .page-root {
   display: flex;
   flex-direction: column;
+  overflow: hidden;
 }
 
 #top-pane {

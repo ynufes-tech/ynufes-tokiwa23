81aaa7c0c224ec2fa9640a0b4ffd7b19629f9447
tomoyahiroe <tomoya4creative@gmail.com>
2023-09-06 11:32:47 +0900

    雑に声優ページを実装(リファクタ必須)

---
+++ layouts/voice-actor.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="layout-root">
+    <LogoPrime />
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.layout-root {
+  min-width: 100svw;
+  min-height: 100vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(/images/sp-background.webp) no-repeat center center fixed;
+  .logo-prime {
+    color: white;
+    margin: 0;
+
+    > h1 {
+      color: white !important;
+    }
+    > hr {
+      border-color: white !important;
+    }
+    width: fit-content;
+  }
+}
+</style>

0a8104c2722685b45ee9fd477962eda9a9b99029
Ito Hayato <131941304+hayato19@users.noreply.github.com>
2023-09-25 14:30:26 +0900

    協賛企業・個人一覧ページの構成の配置

--- pages/sponsors/index.vue
+++ pages/sponsors/index.vue
@@ -13,12 +13,54 @@
         <div class="sponsors-about-contents">
             <p class="text">感謝の文</p>
         </div>
-        </div>
-        <SponsorsWaveLine/>
-        <div class="sponsors-list">
-
-        </div>
+            <div class="banner-ads">
+                <p>バナー広告</p>
             </div>
+            <SponsorsWaveLine/>
+        </div>
+        <div class="sponsors-list">
+            <div class="ads-sponsor">
+                <SponsorsListTitle text="広告協賛"/>
+                <ul class="ads-sponsors-list">
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                </ul>
+                <SponsorsWaveLine/>
+            </div>
+            <div class="donation">
+                <SponsorsListTitle text="寄付"/>
+                <ul class="donation-sponsors-list">
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                </ul>
+                <SponsorsWaveLine/>
+            </div>
+            <div class="booth-sponsor">
+                <SponsorsListTitle text="ブース協賛"/>
+                <ul class="booth-sponsors-list">
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                </ul>
+                <SponsorsWaveLine/>
+            </div>
+            <div class="goods-sponsor">
+                <SponsorsListTitle text="物品協賛"/>
+                <ul class="goods-sponsors-list">
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                </ul>
+                <SponsorsWaveLine/>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 

39b356eb2b542f4ecf7e852e12213c8d997293cf
Ito Hayato <131941304+hayato19@users.noreply.github.com>
2023-09-25 12:54:39 +0900

    サブタイトルと波線のコンポーネントの実装

---
+++ components/SponsorsListTitle.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <div class="sponsors-list-title">
+    <h2>{{ props.text }}</h2>
+  </div>
+</template>
+
+<style scoped>
+.sponsors-list-title {
+  background: url("/images/sponsors/sponsors-list-title.webp") no-repeat left/contain;
+  width: min(768px, 100svw);
+  height: 130px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+
+  > h2 {
+    font-size: min(2.5rem, 5svw);
+    font-weight: bold;
+    margin: 0 0 0 25%;
+    color: var(--thick-font-color);
+  }
+}
+</style>
---
+++ pages/sponsors/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="sponsors-page">
+<div class="sponsors-title">
+    <PageTitle title="SPONSORS"/>
+</div>
+    <div class="inner-sponsors-page">
+        <div class="sponsors-about">
+            <section-title text="ご協賛について" class="upper-title"/>
+        <div class="sponsors-about-contents">
+            <p class="text">感謝の文</p>
+        </div>
+        </div>
+        <SponsorsWaveLine/>
+        <div class="sponsors-list">
+
+        </div>
+            </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>

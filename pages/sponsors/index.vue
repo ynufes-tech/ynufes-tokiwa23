52756b095347f304003339412a907a3258397759
Ito Hayato <131941304+hayato19@users.noreply.github.com>
2023-09-27 22:44:14 +0900

    「現状の」データの注入

--- pages/sponsors/index.vue
+++ pages/sponsors/index.vue
@@ -163,8 +163,11 @@
             text-align: center;
             color: var(--thick-font-color);
         }
-        @media screen and (max-width: 768px) {
+        @media screen and (max-width: 960px){
             width: 80vw;
+        }
+        @media screen and (max-width: 768px) {
+            width: 90vw;
             > li {
                 width: 100%;
             }

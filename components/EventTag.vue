eaaf48c0eba7156c5a532b36d03bce1f93623b14
shuma-I <rns07k@gmail.com>
2023-10-07 14:32:07 +0900

    イベントタグコンポーネントの雛形の作成

---
+++ components/EventTag.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {EventType} from "~/.nuxt/types/event";
+
+export type Props = {
+  eventType: EventType
+}
+const props = defineProps<Props>()
+
+function getTagColor(type: EventType): string {
+  switch (type) {
+    case EventType.FOOD:
+      return "#FFC107"
+    case EventType.EXHIBITION:
+      return "#FF5722"
+    case EventType.PERFORMANCE:
+      return "#4CAF50"
+  }
+  return "#000000"
+}
+</script>
+
+<template>
+  {{ getTagColor(props.eventType) }}
+</template>
+
+<style scoped>
+
+</style>

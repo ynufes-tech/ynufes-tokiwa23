2061a19f2d091d319f7704dae55d1a9e32c06041
Ito Hayato <131941304+hayato19@users.noreply.github.com>
2023-10-19 10:32:44 +0900

    Revert "ページの幅の上限を指定　の打消し"
    This reverts commit 0ce20bab413235d90db3f770777e5f101dc6425e.

--- pages/pamphlet/index.vue
+++ pages/pamphlet/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 import BackToHome from "~/components/BackToHome.vue";
 useHead({
   title: "学内マップ&パンフレット | 23常盤祭公式HP~未来航路~",
@@ -6,168 +7,136 @@ useHead({
 </script>
 
 <template>
-  <div class="page-root">
-    <PageTitle title="学内マップ&amp;パンフレット" />
-    <div class="inner-map">
-      <SectionTitle text="学内MAP" class="upper-title" />
-      <div class="inner-map-items">
-        <p class="inner-map-text">
-          PDFファイルはこちらからダウンロードできます
-        </p>
-        <a
-          herf="https://www.ynu.ac.jp/about/public/publish/ynumap/pdf/ynumap_2022.pdf"
-        >
-          <div class="map-download">
-            <p>・学内MAPのダウンロード</p>
-            <img src="/images/pamphlet/PDF-icon.webp" alt="" class="pdf-icon" />
-          </div>
-          <img class="map-img" src="/images/pamphlet/map-face.webp" alt="" />
-        </a>
-      </div>
-    </div>
-    <div class="inner-pamphlet">
-      <SectionTitle text="常盤祭パンフレット" class="upper-title" />
-      <div class="pamphlet-items">
-        <img
-          src="/images/pamphlet/pamphlet-face.webp"
-          alt="pamphlet"
-          class="pamphlet-img"
-        />
-        <div class="pamphlet-text">
-          <p>下のボタンからパンフレットをダウンロードできます</p>
-          <div class="pamphlet-download">
-            <p>通常版（ーーM)</p>
-            <a href="" class="pamphlet-button">
-              <DownloadButton />
-            </a>
-          </div>
-          <div class="pamphlet-download">
-            <p>軽量版（ーーM）</p>
-            <a href="" class="pamphlet-button">
-              <DownloadButton />
-            </a>
-          </div>
+    <div class="page-root">
+            <PageTitle title="学内マップ&amp;パンフレット"/>
+        <div class="inner-map">
+            <SectionTitle text="学内MAP" class="upper-title"/>
+            <div class="inner-map-items">
+                <p class="inner-map-text">PDFファイルはこちらからダウンロードできます</p>
+                <a herf="https://www.ynu.ac.jp/about/public/publish/ynumap/pdf/ynumap_2022.pdf">
+                    <div class="map-download">
+                        <p>・学内MAPのダウンロード</p>
+                        <img src="/images/pamphlet/PDF-icon.webp" alt="" class="pdf-icon"/>
+                    </div>
+                    <img class="map-img" src="/images/pamphlet/map-face.webp" alt=""/>
+                </a>
+
+            </div>
         </div>
-      </div>
+        <div class="inner-pamphlet">
+            <SectionTitle text="常盤祭パンフレット" class="upper-title"/>
+            <div class="pamphlet-items">
+                <img src="/images/pamphlet/pamphlet-face.webp" alt="pamphlet" class="pamphlet-img"/>
+                <div class="pamphlet-text">
+                    <p>下のボタンからパンフレットをダウンロードできます</p>
+                    <div class="pamphlet-download">
+                        <p>通常版（ーーM)</p>
+                        <a href="" class="pamphlet-button">
+                            <DownloadButton/>
+                        </a>
+                    </div>
+                    <div class="pamphlet-download">
+                        <p>軽量版（ーーM）</p>
+                        <a href="" class="pamphlet-button">
+                            <DownloadButton/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-button">
+            <BackToHome/>
+        </div>
     </div>
-    <div class="bottom-button">
-      <BackToHome />
-    </div>
-  </div>
 </template>
 
 <style scoped lang="scss">
-.inner-map {
-  color: var(--thick-font-color);
-  width: min(1024px, 100svw);
-  margin: 0 auto;
-
-  .inner-map-items {
-    width: 80svw;
-    margin-left: 10svw;
-
-    .map-img {
-      width: min(80svw, 820px);
+.inner-map{
+    color: var(--thick-font-color);
+    .inner-map-items{
+        width: 80svw;
+        margin-left: 10svw;
+        .map-img{
+            width: 100%;
+        }
+        .inner-map-text{
+            margin: 2em 0;
+        }
+        .map-download{
+            display: flex;
+            margin: 1em 0;
+            > p {
+                text-decoration: none;
+                color: var(--thick-font-color);
+            }
+            > img{
+                width: 1em;
+            }
+        }
     }
-
-    .inner-map-text {
-      margin: 2em 0;
-    }
-
-    .map-download {
-      display: flex;
-      margin: 1em 0;
-
-      > p {
-        text-decoration: none;
-        color: var(--thick-font-color);
-      }
-
-      > img {
-        width: 1em;
-      }
-    }
-  }
 }
-
-.inner-pamphlet {
-  width: min(1024px, 100svw);
-  margin: 0 auto;
-
-  .pamphlet-items {
-    display: flex;
-    margin-top: 2em;
-
-    .pamphlet-img {
-      width: 40svw;
-      height: 31.46svw; /*縦横比固定*/
-    }
-
-    .pamphlet-text {
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      margin-left: 10%;
-      width: 40svw;
-      margin-top: 2em;
-
-      .pamphlet-download {
-        margin-top: 3em;
+.inner-pamphlet{
+    .pamphlet-items{
         display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-
-        .pamphlet-button {
-          color: var(--thick-font-color);
-          text-decoration: none;
+        width: 80svw;
+        margin-top: 2em;
+        margin-left: 10svw;
+        .pamphlet-img{
+            width: 40svw;
+            height: 31.46svw;/*縦横比固定*/
         }
-
-        > p {
-          margin-bottom: 1em;
+        .pamphlet-text{
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin-left: 10%;
+            width: 40svw;
+            margin-top: 2em;
+            .pamphlet-download{
+                margin-top: 3em;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                .pamphlet-button{
+                    color: var(--thick-font-color);
+                    text-decoration: none;
+                }
+                > p {
+                    margin-bottom: 1em;
+                }
+            }
         }
-      }
     }
-  }
 }
-
-.bottom-button {
-  margin-top: 2em;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-@media screen and (max-width: 1024px) {
-  .pamphlet-items {
-    margin-left: 10svw;
-  }
-}
-
-@media screen and (max-width: 768px) {
-  .inner-pamphlet {
-    .pamphlet-items {
-      width: 100svw;
-      margin-left: 0;
-      flex-direction: column;
+.bottom-button{
+      margin-top: 2em;
+      display: flex;
       justify-content: center;
       align-items: center;
-
-      .pamphlet-text {
-        margin-left: 0;
-        width: 100%;
-
-        .pamphlet-download {
-          margin-top: 1em;
-        }
-
-        > p {
-          text-align: center;
-          width: 80svw;
-          margin-left: 10svw;
-        }
-      }
-    }
-  }
 }
+@media screen and (max-width: 768px){
+    .inner-pamphlet{
+        .pamphlet-items{
+            width: 100svw;
+            margin-left: 0  ;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            .pamphlet-text{
+                margin-left: 0;
+                width: 100%;
+                .pamphlet-download{
+                    margin-top: 1em;
+                }
+                > p {
+                    text-align: center;
+                    width: 80svw;
+                    margin-left: 10svw;
+                }
+            }
+        }
+    }
+}
+
 </style>

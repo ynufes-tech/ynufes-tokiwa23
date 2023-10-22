9119fc01f1c11b1908b24d308bb1a908b6110da8
Shion Ichikawa <shion1305@gmail.com>
2023-10-22 21:43:17 +0900

    🎨 (component/Countdown)CountdownをHomeWidgetより生成

--- components/Countdown.vue
+++ components/Countdown.vue
@@ -1,36 +1,37 @@
 <script lang="ts" setup>
-
 // 当日までの時間を取得
 const eventTime = new Date(`November 03 2023 13:00:00`).getTime();
 onMounted(() => {
-  const days = document.getElementById('days')!!;
-  const hours = document.getElementById('hours')!!;
-  const minutes = document.getElementById('minutes')!!;
-  const seconds = document.getElementById('seconds')!!;
+  const days = document.getElementById("days")!!;
+  const hours = document.getElementById("hours")!!;
+  const minutes = document.getElementById("minutes")!!;
+  const seconds = document.getElementById("seconds")!!;
 
   updateCountdown({
-    days, hours, minutes, seconds
-  })
-
-// 1秒ごとに関数を実行
-  setInterval(updateCountdown, 1000, {
-    days, hours, minutes, seconds
+    days,
+    hours,
+    minutes,
+    seconds,
   });
-})
+
+  // 1秒ごとに関数を実行
+  setInterval(updateCountdown, 1000, {
+    days,
+    hours,
+    minutes,
+    seconds,
+  });
+});
 
 // 要素を取得
 
-
 // カウントダウンの関数
-function updateCountdown(
-  elements:
-    {
-      days: HTMLElement,
-      hours: HTMLElement,
-      minutes: HTMLElement,
-      seconds: HTMLElement,
-    }
-) {
+function updateCountdown(elements: {
+  days: HTMLElement;
+  hours: HTMLElement;
+  minutes: HTMLElement;
+  seconds: HTMLElement;
+}) {
   // 現在の時刻情報を取得する
   const currentTime = new Date().getTime();
   // 現在の時間と当日との時間の差を計算する（ミリ秒単位）
@@ -47,38 +48,43 @@ function updateCountdown(
 
   // 取得した時間をDOMに追加
   elements.days.innerText = String(d);
-  elements.hours.innerText = String(h < 10 ? '0' + h : h);
-  elements.minutes.innerText = String(m < 10 ? '0' + m : m);
-  elements.seconds.innerText = String(s < 10 ? '0' + s : s);
+  elements.hours.innerText = String(h < 10 ? "0" + h : h);
+  elements.minutes.innerText = String(m < 10 ? "0" + m : m);
+  elements.seconds.innerText = String(s < 10 ? "0" + s : s);
 }
-
 </script>
 
 <template>
-  <div class="countdown">
-    <h1 class="countdown-title">常盤祭2023</h1>
-    <h2 class="countdown-sub-title">まで...</h2>
-    <div class="time-container">
-      <div class="time"><p><span id="days">--</span>days</p></div>
-      <div class="time"><p><span id="hours">--</span>hours</p></div>
-      <div class="time"><p><span id="minutes">--</span>minutes</p></div>
-      <div class="time"><p><span id="seconds">--</span>seconds</p></div>
-    </div>
-  </div>
+  <HomeWidget>
+    <template #title>
+      <h1 class="countdown-title">常盤祭2023</h1>
+      <h2 class="countdown-sub-title">まで...</h2>
+    </template>
+    <template #content>
+      <div class="time-container">
+        <div>
+          <p><span id="days">--</span>days</p>
+        </div>
+        <div>
+          <p><span id="hours">--</span>hours</p>
+        </div>
+        <div>
+          <p><span id="minutes">--</span>minutes</p>
+        </div>
+        <div>
+          <p><span id="seconds">--</span>seconds</p>
+        </div>
+      </div>
+    </template>
+  </HomeWidget>
 </template>
 
 <style lang="scss" scoped>
-.countdown {
-  color: #444455;
-  text-align: center;
-  width: max(340px, 35svw);
-}
-
 .countdown-title {
   font-size: 1.8rem;
   font-weight: bold;
   padding: 30px 0 0 0;
-  background-color: #8CB6DE;
+  background-color: #8cb6de;
   border-radius: 25px 25px 0 0;
 }
 
@@ -86,7 +92,7 @@ function updateCountdown(
   text-indent: 40%;
   font-size: 1rem;
   font-weight: bolder;
-  background-color: #8CB6DE;
+  background-color: #8cb6de;
   padding: 15px 0;
   text-align: center;
 }
@@ -95,12 +101,12 @@ function updateCountdown(
 .time-container {
   display: flex;
   justify-content: center;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   border-radius: 0 0 25px 25px;
   width: 100%;
 }
 
-.time {
+.time-container > div {
   width: 25%;
 
   /* hours, minutes, secondsの共通スタイル */
@@ -109,7 +115,6 @@ function updateCountdown(
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin: 40px 15px;
     font-size: 12px;
     font-weight: bold;
 
@@ -120,5 +125,4 @@ function updateCountdown(
     }
   }
 }
-
 </style>

856f5225c61124315e39164e23226bb155794771
Shion Ichikawa <shion1305@gmail.com>
2023-10-23 20:32:19 +0900

    🚨 (pages/events)run formatter

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
 import events from "~/assets/data/events.json";
+
 useHead({
   title: "企画団体紹介ページ",
   meta: [
     {
       name: "description",
-      content:
-        "企画団体の紹介をしております！",
+      content: "企画団体の紹介をしております！",
     },
   ],
 });
@@ -14,33 +14,34 @@ useHead({
 
 <template>
   <div class="events">
-    <PageTitle title="EVENTS"/>
+    <PageTitle title="EVENTS" />
     <div class="layout">
-      <SectionTitle text="企画団体"/>
+      <SectionTitle text="企画団体" />
       <div class="card-wrapper">
         <NuxtLink
           v-for="event in events"
-          :to="`/event/${event.id}`"
           :key="event.id"
+          :to="`/event/${event.id}`"
           class="card"
         >
-        <ProjectCard :event-data="event" />
+          <ProjectCard :event-data="event" />
         </NuxtLink>
       </div>
     </div>
   </div>
 </template>
 
-<style  lang="scss" scoped>
-.layout{
+<style lang="scss" scoped>
+.layout {
   margin: 0 auto;
   max-width: 1024px;
 }
-.card-wrapper{
+
+.card-wrapper {
   margin-top: 100px;
   justify-content: center;
   background-position: initial;
-  background-image:url(/images/background.png) ;
+  background-image: url(/images/background.png);
   background-repeat: repeat-y;
   background-size: contain;
   background-clip: border-box;
@@ -48,9 +49,9 @@ useHead({
   flex-direction: row;
   flex-wrap: wrap;
   gap: 40px;
-  
 }
-.card{
+
+.card {
   margin-top: 100px;
 }
 </style>

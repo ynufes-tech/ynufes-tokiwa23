1a78f1f1779da1c95f13f86a789d2f6a42e7c3eb
Shion Ichikawa <shion1305@gmail.com>
2023-11-02 12:50:11 +0900

    ✨ 企画コンテスト用のAPI完成

---
+++ server/api/contest.ts
@@ -0,0 +1,34 @@
+let rankingData: any = null;
+
+let updatedTime = 0;
+
+const contest_url = useRuntimeConfig().contestDataUrl;
+if (!contest_url) {
+  console.error("CONTEST_DATA_URL is not configured");
+}
+
+export default defineEventHandler(async (event) => {
+  if (!contest_url) {
+    return {};
+  }
+  const timeNow = Date.now();
+  if (!rankingData) {
+    await execUpdate();
+    updatedTime = timeNow;
+    return rankingData;
+  }
+  if (timeNow - updatedTime > 1000 * 60) {
+    updatedTime = timeNow;
+    execUpdate();
+    return rankingData;
+  }
+});
+
+async function execUpdate() {
+  const resp = await $fetch(contest_url).catch(() => null);
+  if (!resp) {
+    console.error("Failed to fetch contest data");
+    return;
+  }
+  rankingData = resp;
+}

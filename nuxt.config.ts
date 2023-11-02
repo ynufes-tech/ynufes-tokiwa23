f767349d23e98e7b3406ed2656d589ac8bbb787f
Ito Hayato <131941304+hayato19@users.noreply.github.com>
2023-11-03 08:30:05 +0900

    initialConsent、IS_PRODUCTIONの追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
       "nuxt-gtag",
       {
         id: process.env.GOOGLE_ANALYTICS_ID,
+        initialConsent: process.env.IS_PRODUCTION,
       },
     ],
   ],
